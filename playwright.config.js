cfde3aa79aa13a579649d07fdbc7c992e6ab26f5
Botcha  Saikrishna <saikrishna.b@bourntec.com>
2025-06-06 14:21:10 +0530

    Testing purpose

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ const config = defineConfig({
 
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     screenshot: 'on',
     trace: 'on',
     video: 'off',
---
+++ tests/Test_demo.spec.js
@@ -0,0 +1,19 @@
+import {test,expect} from "@playwright/test";
+
+test('Test_demo', async({page}) => {
+
+    await page.goto('https://opencart.abstracta.us/')
+    await page.click('a[href="http://opencart.abstracta.us:80/index.php?route=product/category&path=24"]')
+    await page.click('//a[text()="HTC Touch HD"]');
+    const Input_quantity = await page.locator('//input[@id="input-quantity"]');
+    await Input_quantity.fill('2');
+    await page.click('#button-cart');
+    await page.click('#cart-total');
+    await page.waitForTimeout(2000);
+    await page.click('a:has-text("Checkout")');
+    await page.fill('[name="email"]', 'test@example.com');
+    await page.fill('[name="password"]', '12345678');
+    await page.click('#button-login');
+
+
+})
