d17e0e2ddf4589b443a916a7541fc0c76cacb674
Botcha  Saikrishna <saikrishna.b@bourntec.com>
2025-06-17 13:13:09 +0530

    Assertions

--- tests/Test_demo.spec.js
+++ tests/Test_demo.spec.js
@@ -3,6 +3,11 @@ import {test,expect} from "@playwright/test";
 test('opencart', async({page}) => {
 
     await page.goto('https://opencart.abstracta.us/')
+    await expect(page).toHaveTitle(/Your Store/);
+    // Assert search bar is present and enabled
+    await expect(page.locator('input[name="search"]')).toBeVisible();
+    // Assert top links are visible (My Account)
+    await expect(page.getByRole('link', { name: 'My Account', exact: true })).toBeVisible();
     await page.click('a[href="http://opencart.abstracta.us:80/index.php?route=product/category&path=24"]')
     await page.click('//a[text()="HTC Touch HD"]');
     await page.waitForTimeout(2000);
